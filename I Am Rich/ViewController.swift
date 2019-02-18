40a9090434917599568b7b40ad1c289c1837540b
rudi wijaya <rudiwijaya@rudis-Mac-mini.local>
2019-02-18 09:56:11 +0700

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Rudi Wijaya on 18/02/19.
+//  Copyright © 2019 Rudi Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
